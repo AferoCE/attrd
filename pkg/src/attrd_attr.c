7f12c247ca64eb4508519b53d090e9a20d7b4a98
Clif Liu <clif@afero.io>
2017-08-03 16:39:31 -0700

    Support system time attribute
    Change-Id: I0df2d18ad75aef62653a1db2d3126fea5ad6d92b

--- pkg/src/attrd_attr.c
+++ pkg/src/attrd_attr.c
@@ -55,6 +55,16 @@ void handle_attrd_get_request(uint32_t seqNum, uint16_t getId, uint32_t attrId)
             AFLOG_INFO("attrd_report_rssi_changes_get:reportChanges=%d", sReportRssiChanges);
             send_attrd_get_response(AF_ATTR_STATUS_OK, seqNum, getId, (uint8_t *)&sReportRssiChanges, sizeof(sReportRssiChanges));
             break;
+        case AF_ATTR_ATTRD_SYSTEM_TIME :
+        {
+            struct timeval tv;
+            uint8_t buf[sizeof(tv.tv_sec)];
+            gettimeofday(&tv, NULL);
+            AFLOG_INFO("attrd_system_time_get:time=%d", (int)tv.tv_sec);
+            af_attr_store_int32(buf, tv.tv_sec);
+            send_attrd_get_response(AF_ATTR_STATUS_OK, seqNum, getId, buf, sizeof(tv.tv_sec));
+            break;
+        }
         default :
             break;
     }
