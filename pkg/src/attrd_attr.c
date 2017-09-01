faab292b34f204c710b5f734cc837600205715ef
Clif Liu <clif@afero.io>
2017-09-01 16:21:12 -0700

    Support reboot reason attribute
    Change-Id: I78c0c28e368e20ff89d0e1d4fbaa80441b756b87

--- pkg/src/attrd_attr.c
+++ pkg/src/attrd_attr.c
@@ -4,6 +4,12 @@
  * Copyright (c) 2017 Afero, Inc. All rights reserved.
  *
  */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <event2/event.h>
 #include "af_log.h"
 #include "af_attr_client.h"
@@ -41,6 +47,56 @@ int handle_attrd_set_request(uint32_t attrId, uint8_t *data, int size)
     return retVal;
 }
 
+#define REBOOT_REASON_FILE_PATH "/afero_nv/reboot_reason"
+
+static void handle_reboot_reason_get_request(uint32_t seqNum, uint16_t getId)
+{
+    /* check if the reboot reason file exists */
+    struct stat st;
+    if (stat(REBOOT_REASON_FILE_PATH, &st) < 0) {
+        if (errno == ENOENT) {
+            send_attrd_get_response(AF_ATTR_STATUS_OK, seqNum, getId, (uint8_t *)"", 1);
+        } else {
+            AFLOG_ERR("attrd_reboot_reason_stat:errno=%d", errno);
+        }
+
+        return;
+    }
+
+    /* capture the reboot reason file modification time */
+    time_t utc = st.st_mtime;
+    /* ctime uses a shared buffer. I don't like it so I copy it away immediately */
+    char dateBuf[32];
+    strcpy(dateBuf, ctime(&utc)); /* format is "Www Mmm Dd hh:mm:ss yyyy\n" : 25 chars + nul char */
+    /* chop off the \n from the date buffer */
+    int nc = strlen(dateBuf);
+    if (nc > 0) {
+        dateBuf[nc - 1] = '\0';
+    }
+
+    char contents[100];
+
+    int fd = open(REBOOT_REASON_FILE_PATH, O_RDONLY);
+    if (fd < 0) {
+        AFLOG_ERR("attrd_reboot_reason_open:errno=%d", errno);
+        return;
+    }
+
+    nc = read(fd, contents, sizeof(contents) - 1);
+    close(fd);
+
+    if (nc < 0) {
+        AFLOG_ERR("attrd_reboot_reason_read:errno=%d", errno);
+        return;
+    }
+    contents[nc] = '\0';
+
+    char reason[100];
+    nc = snprintf(reason, sizeof(reason), "%s:%s", dateBuf, contents);
+
+    send_attrd_get_response(AF_ATTR_STATUS_OK, seqNum, getId, (uint8_t *)reason, nc);
+}
+
 void handle_attrd_get_request(uint32_t seqNum, uint16_t getId, uint32_t attrId)
 {
     switch (attrId) {
@@ -65,6 +121,10 @@ void handle_attrd_get_request(uint32_t seqNum, uint16_t getId, uint32_t attrId)
             send_attrd_get_response(AF_ATTR_STATUS_OK, seqNum, getId, buf, sizeof(tv.tv_sec));
             break;
         }
+        case AF_ATTR_ATTRD_REBOOT_REASON :
+            handle_reboot_reason_get_request(seqNum, getId);
+            break;
+
         default :
             break;
     }
